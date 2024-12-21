76d669238e6c20cba8eb7adfa3bbf1fa9b97814d
TTRDpit <112976358+TimaDub@users.noreply.github.com>
2024-12-21 08:03:06 +0100

    Update app.py

--- prototypes/app.py
+++ prototypes/app.py
@@ -11,23 +11,30 @@ def home_page():
         "Disk A": ["Item 1.1", "Item 1.2", "Item 1.3"],
         "Disk B": ["Item 2.1", "Item 2.2"],
         "Disk C": ["Item 3.1", "Item 3.2", "Item 3.3", "Item 3.4"],
-        "Disk D": ["Item 5.1", "Item 5.2", "Item 5.3", "Item 5.4", "Item 5.5"]
+        "Disk D": ["Item 5.1", "Item 5.2", "Item 5.3", "Item 5.4", "Item 5.5"],
     }
-    
+
     pie_data = ["1000", "980", "453", "1000", "980", "453"]
     pie_labels = [".py", ".pyc", ".txt", ".png", ".jpg"]
-    
+
     pie_total_space = "100.0 GB"
-    
-    return render_template("current_disk_page.html", data=data, pie_data=pie_data, pie_labels=pie_labels, pie_total_space=pie_total_space)
+
+    return render_template(
+        "current_disk_page.html",
+        data=data,
+        pie_data=pie_data,
+        pie_labels=pie_labels,
+        pie_total_space=pie_total_space,
+    )
 
 
 @app.route("/exit")
 def exit():
     if hasattr(window, "destroy"):
         window.destroy()
-    
-    return ''
+
+    return ""
+
 
 window = webview.create_window(
     "Flask app",
